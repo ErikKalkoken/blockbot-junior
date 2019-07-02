263d012379e7f5e42f5c674492c9a61d7fd13339
Erik Kalkoken <erik.kalkoken@gmail.com>
2019-07-02 22:52:29 +0200

    removed IDs

--- config.py
+++ config.py
@@ -2,14 +2,10 @@
 
 # list of users that can see configuration of a channel
 # and speak in all announcement channels
-ADMIN_USERS = [
-   "U0HBXN3H8"
+ADMIN_USERS = [   
 ]
 
 # list of users than can post in a specific announcement channel
-ALLOWED_CHANNEL_USERS = {
-   "C3E2JRGQK": [
-      "U0UGVN9GF"
-   ]
+ALLOWED_CHANNEL_USERS = {   
 }
 
